9fb93cc3092b02df75b8a7f2d22ef50ece11a016
dizzydevil <stephan_lesch@users.sourceforge.net>
2021-08-17 14:29:06 +0200

    [bugfix] Correct the verify error offset in the verify function for serial flash.

--- src/flasher_spi.c
+++ src/flasher_spi.c
@@ -249,7 +249,7 @@ static NETX_CONSOLEAPP_RESULT_T spi_verify_with_progress(const FLASHER_SPI_FLASH
 		{
 			if( pucCmp0[sizCmpCnt]!=pucCmp1[sizCmpCnt] )
 			{
-				uprintf(". verify error at offset 0x%08x. buffer: 0x%02x, flash: 0x%02x.\n", ulC + ulProgressCnt + sizCmpCnt, pucCmp1[sizCmpCnt], pucCmp0[sizCmpCnt]);
+				uprintf(". verify error at offset 0x%08x. buffer: 0x%02x, flash: 0x%02x.\n", ulC + sizCmpCnt, pucCmp1[sizCmpCnt], pucCmp0[sizCmpCnt]);
 				return NETX_CONSOLEAPP_RESULT_ERROR;
 			}
 			++sizCmpCnt;
