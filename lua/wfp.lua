49e822f2aeaf39a02d350b7bde9727ab24573e68
fwoermann <fwoermann@hilscher.com>
2021-10-28 15:57:54 +0200

    [code] Handle WFPs with multiple files with the same name. (NXTFLASHER-246)
    Change the way of saving information of files from wfp archive to enable names with the same name in subdirectories.
    
    (cherry picked from commit 3c5ccba18666d407bd1ac704e2e46167c836f669)

--- lua/wfp.lua
+++ lua/wfp.lua
@@ -562,7 +562,7 @@ elseif tArgs.fCommandPackSelected == true then
                                     tLog.debug('Extending the relative path "%s" to "%s".', strFile, strFileAbs)
                                 end
                                 local strFileBase = pl.path.basename(strFile)
-                                if atFiles[strFileBase] == nil then
+                                if atFiles[strFile] == nil then
                                     if pl.path.exists(strFileAbs) ~= strFileAbs then
                                         tLog.error('The path "%s" does not exist.', strFileAbs)
                                         fOk = false
@@ -571,7 +571,7 @@ elseif tArgs.fCommandPackSelected == true then
                                         fOk = false
                                     else
                                         tLog.debug('Adding file "%s" to the list.', strFileAbs)
-                                        atFiles[strFileBase] = strFileAbs
+                                        atFiles[strFile] = strFileAbs
                                         local tAttr = {
                                             ucBus = tBus,
                                             ucUnit = tTargetFlash.ulUnit,
@@ -582,8 +582,8 @@ elseif tArgs.fCommandPackSelected == true then
                                         }
                                         table.insert(atSortedFiles, tAttr)
                                     end
-                                elseif atFiles[strFileBase] ~= strFileAbs then
-                                    tLog.error('Multiple files with the base name "%s" found.', strFileBase)
+                                elseif atFiles[strFile] ~= strFileAbs then
+                                    tLog.error('Multiple files with the path "%s" found.', strFileBase)
                                     fOk = false
                                 end
                             end
