b8f9b294a2432e970e1c948f1514fbcb66121f38
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-08-23 17:59:02 +0200

    Whitespace, linefeeds and UTF-8 encoding.

--- lua/flasher_test.lua
+++ lua/flasher_test.lua
@@ -1,7 +1,7 @@
 module("flasher_test", package.seeall)
 
 ---------------------------------------------------------------------------
--- Copyright (C) 2019 Hilscher Gesellschaft für Systemautomation mbH
+-- Copyright (C) 2019 Hilscher Gesellschaft fÃ¼r Systemautomation mbH
 --
 -- Description:
 --   flasher_test.lua: flasher test routines
@@ -39,18 +39,18 @@ ulDeviceSizeMax = 0x8000000
 -- Generally, the routines return either true or false and an error message.
 -- Read routines return the data or nil and an error message.
 -- isErased returns true or false and an error message.
--- 
+--
 -- fOk, strMsg      init()            -- open plugin, download flasher, detect chip etc.
 --
 -- ulSize           getDeviceSize()   -- returns chip size in bytes
 -- ulBusWidth       getBusWidth()     -- returns 1, 2 or 4
 --
--- fOk, strMsg      flash(ulOffset, strData) 
+-- fOk, strMsg      flash(ulOffset, strData)
 -- fOk, strMsg      verify(ulOffset, strData)
 -- strData, strMsg  read(ulOffset, ulSize)   -- returns data or nil and an error message
--- fOk, strMsg      erase(ulOffset, ulSize) 
--- fOk, strMsg      isErased(ulOffset, ulSize) 
--- 
+-- fOk, strMsg      erase(ulOffset, ulSize)
+-- fOk, strMsg      isErased(ulOffset, ulSize)
+--
 -- strData, strMsg  readChip()
 -- fOK, strMsg      eraseChip()
 -- fOk, strMsg      isChipErased()
@@ -89,7 +89,7 @@ function flasher_interface.init(self)
 	-- check if the selected flash is present
 	print("Detecting flash device")
 	fOk = flasher.detect(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		self.iBus, self.iUnit, self.iChipSelect,
 		self.fnCallbackMessage, self.fnCallbackProgress
 		)
@@ -105,9 +105,9 @@ end
 
 function flasher_interface.getDeviceSize(self)
 	local ulSize = flasher.getFlashSize(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		self.fnCallbackMessage, self.fnCallbackProgress)
-		
+
 	if ulSize then
 		if ulSize > ulDeviceSizeMax then
 			ulSize = ulDeviceSizeMax
@@ -148,28 +148,28 @@ end
 
 function flasher_interface.flash(self, ulOffset, strData)
 	return flasher.flashArea(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		ulOffset, strData,
 		self.fnCallbackMessage, self.fnCallbackProgress)
 end
 
 function flasher_interface.verify(self, ulOffset, strData)
 	return flasher.verifyArea(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		ulOffset, strData,
 		self.fnCallbackMessage, self.fnCallbackProgress)
 end
 
 function flasher_interface.read(self, ulOffset, ulSize)
 	return flasher.readArea(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		ulOffset, ulSize,
 		self.fnCallbackMessage, self.fnCallbackProgress)
 end
 
 function flasher_interface.erase(self, ulOffset, ulSize)
 	return flasher.eraseArea(
-		self.tPlugin, self.aAttr, 
+		self.tPlugin, self.aAttr,
 		ulOffset, ulSize,
 		self.fnCallbackMessage, self.fnCallbackProgress)
 end
@@ -180,7 +180,7 @@ function flasher_interface.isErased(self, ulOffset, ulSize)
 	local fIsErased = flasher.isErased(
 		self.tPlugin, self.aAttr, ulOffset, ulOffset + ulSize,
 		self.fnCallbackMessage, self.fnCallbackProgress)
-		
+
 	return fIsErased, fIsErased and "The area is empty" or "The area is not empty"
 end
 
@@ -228,14 +228,14 @@ end
 
 -- insert a random segment in unused space.
 -- segments must be ordered by offset and non-overlapping
--- iWordSize: round addresses to 1/2/4 bytes 
+-- iWordSize: round addresses to 1/2/4 bytes
 -- returns true if a segment was inserted, false otherwise
 
 function insert_random_segment(atSegments, ulDeviceSize, iWordSize)
 	-- get a random position
 	-- the new segment is inserted between atSegments[iPos] and atSegments[iPos+1]
 	local iPos = math.random(0, #atSegments)
-	
+
 	-- get the inter-segment space at this position
 	local offset   -- 0-based offset of the inter-segment space
 	local size     -- size of the inter-segment space
@@ -253,21 +253,21 @@ function insert_random_segment(atSegments, ulDeviceSize, iWordSize)
 		offset = atSegments[iPos].offset + atSegments[iPos].size
 		size = atSegments[iPos+1].offset - offset
 	end
-	
+
 	if size > 0 then
 		local offset1 = math.random(offset, offset+size-1)  -- start addr
 		local offset2 = math.random(offset1, offset+size-1) -- end addr (incl)
-		
+
 		offset1 = offset1 - (offset1 % iWordSize)
 		offset2 = offset2 - (offset2 % iWordSize) + (iWordSize-1)
-		
+
 		local size1 = offset2 - offset1 + 1
 		printf("0x%08x+0x%08x --> 0x%08x+0x%08x", offset, size, offset1, size1)
 		local tSegment = {offset = offset1, size = size1}
-	
+
 		table.insert(atSegments, iPos+1, tSegment)
 		return true
-		
+
 	else
 		return false
 	end
@@ -281,19 +281,19 @@ function printf(...) print(string.format(...)) end
 
 
 function testFlasher(tFlasherInterface, fnLogPrintf)
-	
+
 	tFlasherInterface = tFlasherInterface or flasher_interface
 	local log_printf = fnLogPrintf or log_printf
-	
+
 	-- init flasher
 	fOk, strMsg = tFlasherInterface:init()
 	assert(fOk, strMsg)
-	
+
 	local ulDeviceSize = tFlasherInterface:getDeviceSize()
-	
+
 	local bEmptyByte = tFlasherInterface:getEmptyByte()
-	
-	
+
+
 	-- for serial flash
 	local atSegments_1={
 		{offset = 0, size = 12345},
@@ -304,17 +304,17 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		{offset = 0x90004, size = 0x10001},
 		{offset = 0xb0004, size = 0x10002},
 		{offset = 0xd0004, size = 0x10003},
-		
+
 		{offset = 0x20000, size = 1},
 		{offset = 0x21004, size = 2},
 		{offset = 0x22008, size = 3},
-		
+
 		{offset = 0x23000, size = 1},
 		{offset = 0x23210, size = 1},
-		
+
 		{offset = ulDeviceSize - 12345, size = 12345},
 	}
-	
+
 	-- for 16 bit parflash:
 	-- offset/size must be multiples of bus width
 	local atSegments_2={
@@ -326,17 +326,17 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		{offset = 0x90004, size = 0x10000},
 		{offset = 0xb0004, size = 0x10002},
 		{offset = 0xd0004, size = 0x10004},
-		
+
 		{offset = 0x20000, size = 2},
 		{offset = 0x21004, size = 4},
 		{offset = 0x22008, size = 6},
-		
+
 		{offset = 0x23000, size = 2},
 		{offset = 0x23210, size = 2},
-		
+
 		{offset = ulDeviceSize - 12346, size = 12346},
 	}
-	
+
 	-- for 32 bit parflash:
 	-- offset/size must be multiples of bus width
 	local atSegments_4={
@@ -345,17 +345,17 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		{offset = 0x30004, size = 0x10000},
 		{offset = 0x50008, size = 0x10000},
 		{offset = 0x6000c, size = 0x10000},
-		
+
 		{offset = 0x20000, size = 4},
 		{offset = 0x21004, size = 8},
 		{offset = 0x2200c, size = 12},
-		
+
 		{offset = 0x23000, size = 4},
 		{offset = 0x23210, size = 4},
-		
+
 		{offset = ulDeviceSize - 12348, size = 12348},
 	}
-    
+
 	-- select the segments list according to the flash type
 	local iBusWidth = tFlasherInterface:getBusWidth()
 	if iBusWidth==1 then
@@ -365,18 +365,18 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 	elseif iBusWidth==4 then
 		atSegments = atSegments_4
 	end
-	
+
 	-- add random segments
 	table.sort(atSegments, function(a, b) return a.offset<b.offset end)
 	math.randomseed(os.time())
 	local iSize = #atSegments
-	
+
 	log_printf("")
 	log_printf("Create random segments:")
 	while #atSegments < iSize + iNumAddSegments do
 		insert_random_segment(atSegments, ulDeviceSize, iBusWidth)
 	end
-	
+
 	log_printf("")
 	log_printf("Segments:")
 	for iSegment, tSegment in ipairs(atSegments) do
@@ -384,10 +384,10 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		local size = tSegment.size
 		log_printf("%d 0x%08x-0x%08x size 0x%x", iSegment, offset, offset+size-1, size)
 	end
-	
+
 	-- reorder
 	atSegments = reorder_randomly(atSegments)
-	
+
 	log_printf("")
 	log_printf("Randomly reordered segments:")
 	for iSegment, tSegment in ipairs(atSegments) do
@@ -395,21 +395,21 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		local size = tSegment.size
 		log_printf("%d 0x%08x-0x%08x size 0x%x", iSegment, offset, offset+size-1, size)
 	end
-	
-	
+
+
 	-- fill segments with data
 	for iSegment, tSegment in ipairs(atSegments) do
 		tSegment.data = tSegment.data or getRandomData(tSegment.size)
 	end
-	
-	
+
+
 	-- erase
 	log_printf("")
 	log_printf("Erase whole flash")
 	fOk, strMsg = tFlasherInterface:eraseChip()
 	log_printf("Result: %s %s", tostring(fOk), tostring(strMsg))
 	assert(fOk, strMsg)
-	
+
 	-- flash the segments
 	log_printf("")
 	log_printf("Flash the segments")
@@ -420,7 +420,7 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		log_printf("Result: %s %s", tostring(fOk), tostring(strMsg))
 		assert(fOk)
 	end
-	
+
 	-- verify the segments
 	log_printf("")
 	log_printf("Verify the segments")
@@ -431,7 +431,7 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		log_printf("Result: %s %s", tostring(fOk), tostring(strMsg))
 		assert(fOk)
 	end
-	
+
 	-- read back
 	log_printf("")
 	log_printf("Read back the segments")
@@ -440,24 +440,24 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		log_printf("Reading Segment %d  offset:0x%08x  size: %d", iSegment, tSegment.offset, tSegment.size)
 		local strData, strMsg = tFlasherInterface:read(tSegment.offset, tSegment.size)
 		log_printf("Read Segment %d  offset:0x%08x  size: %d", iSegment, tSegment.offset, tSegment.size)
-		
+
 		assert(strData, strMsg or "Error reading segment")
-		
+
 		if strData == tSegment.data then
 			log_printf("Segment %d equal", iSegment)
 		else
 			log_printf("Segment %d differs!", iSegment)
 			fOk = false
 		end
-		
+
 	end
 	assert(fOk, "Errors while reading segments")
-	
-	
-	
-	
-	-- Read an image of the whole chip. 
-	-- Check that the data segments have been writen correctly 
+
+
+
+
+	-- Read an image of the whole chip.
+	-- Check that the data segments have been writen correctly
 	-- and that the space in-between is empty (ff)
 
 	-- Read image
@@ -465,21 +465,21 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 	log_printf("Read image")
 	strImage, strMsg = tFlasherInterface:readChip()
 	log_printf("Image read")
-	
+
 	-- Compare the segments and check the space in-between
 	log_printf("")
 	log_printf("Compare the segments")
 	table.sort(atSegments, function(a, b) return a.offset<b.offset end)
-	
+
 	for iSegment, tSegment in ipairs(atSegments) do
 	log_printf("Compare Segment %d with image. offset:0x%08x  size: %d", iSegment, tSegment.offset, tSegment.size)
 		local iStart = tSegment.offset + 1
-		local iEnd   = tSegment.offset + tSegment.size 
+		local iEnd   = tSegment.offset + tSegment.size
 		local iNextStart
-		
+
 		local strData = strImage:sub(iStart, iEnd)
 		assert(strData == tSegment.data, "Segment does not match")
-		
+
 		if iSegment < #atSegments then
 			tSegment = atSegments[iSegment+1]
 			iNextStart = tSegment.offset
@@ -491,10 +491,10 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		for iPos = iEnd+1, iNextStart do
 			assert(strImage:byte(iPos) == bEmptyByte, string.format("0x%08x non-empty", iPos))
 		end
-	
+
 	end
-	
-	
+
+
 	-- Erase the segments
 	log_printf("")
 	log_printf("Erase the segments")
@@ -505,7 +505,7 @@ function testFlasher(tFlasherInterface, fnLogPrintf)
 		log_printf("Result: %s %s", tostring(fOk), tostring(strMsg))
 		assert(fOk)
 	end
-	
+
 	-- the flash should now be empty
 	log_printf("")
 	log_printf("Check emptyness")
