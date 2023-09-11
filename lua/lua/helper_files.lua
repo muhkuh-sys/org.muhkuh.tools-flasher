b36159815611ea166c078702af55037e91c13921
Stephan Lesch <dizzydevil@users.noreply.github.com>
2023-09-11 19:56:18 +0200

    [code] Set the flasher version in helper_files to v2.0.0-dev15
    (cherry picked from commit bf8dda163115444b912007727cce65a72fc9ccd3)

--- lua/lua/helper_files.lua
+++ lua/lua/helper_files.lua
@@ -95,7 +95,7 @@ local atHelperFileVersions = {
     {   -- Todo: Turn this into a template to insert version automatically.
         key = "flasher_netx90_hboot",
         filename = "flasher_netx90_hboot.bin",
-        version = "GITv2.0.0-dev14",
+        version = "GITv2.0.0-dev15",
         version_offset = 0x0410
     },
 
