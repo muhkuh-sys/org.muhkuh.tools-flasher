be25d8406ec46ed4a04766932feb7a9dd4cf7c73
Stephan Lesch <slesch@hilscher.com>
2023-09-21 16:32:49 +0200

    [code] update requires for lua5.4

--- lua/muhkuh_cli_init.lua
+++ lua/muhkuh_cli_init.lua
@@ -5,9 +5,9 @@ require("romloader_uart")
 require("romloader_jtag")
 
 -- Load the common modules for a CLI environment.
-require("muhkuh")
-require("select_plugin_cli")
-require("tester_cli")
+_G.muhkuh = require("muhkuh")
+_G.select_plugin = require("select_plugin_cli")
+_G.tester = require("tester_cli")
 
 -- This string is appended to all paths in the function "load_from_working_folder".
 _G.__MUHKUH_WORKING_FOLDER = ""
