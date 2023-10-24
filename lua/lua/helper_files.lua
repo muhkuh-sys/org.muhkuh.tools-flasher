fb53b7bee1104ff18d89de8ae4f9da3d3b1a4254
Stephan Lesch <dizzydevil@users.noreply.github.com>
2023-10-24 18:41:02 +0200

    [doku][script][code] Set version to 2.0.2

--- lua/lua/helper_files.lua
+++ lua/lua/helper_files.lua
@@ -95,7 +95,7 @@ local atHelperFileVersions = {
     {   -- Todo: Turn this into a template to insert version automatically.
         key = "flasher_netx90_hboot",
         filename = "flasher_netx90_hboot.bin",
-        version = "GITv2.0.0-0-g",
+        version = "GITv2.0.2-0-g",
         version_offset = 0x0410
     },
 
