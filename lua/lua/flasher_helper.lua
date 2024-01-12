061a405bd39e785725bec37118ea555a21e4c018
fwoermann <fwoermann@hilscher.com>
2024-01-12 11:00:46 +0100

    [code] update error message and fix indentation

--- lua/lua/flasher_helper.lua
+++ lua/lua/flasher_helper.lua
@@ -585,7 +585,7 @@ end
 
 function M.detect_secure_boot_mode(aArgs)
   
-  local romloader = require 'romloader'
+	local romloader = require 'romloader'
 	local strPluginName  = aArgs.strPluginName
 	local strPluginType  = aArgs.strPluginType
 	local atPluginOptions= aArgs.atPluginOptions
@@ -720,7 +720,7 @@ function M.detect_secure_boot_mode(aArgs)
       "read_sip_M2M.bin"
     )  --tFlasher.HELPER_FILES_PATH
 		printf("Trying to load netX 90 read_sip_M2M image from %s", strReadSipPath)
-    local strReadSipBin
+		local strReadSipBin
 		strReadSipBin, strMsg = M.loadBin(strReadSipPath)
 		if strReadSipBin == nil then
 			print(strMsg)
@@ -789,7 +789,7 @@ function M.detect_secure_boot_mode(aArgs)
 									print("Failed to reconnect.")
 								else
 									-- read the SIP pages
-                  local tRes
+									local tRes
 									tRes, strMsg = readSip_via_jtag(tPlugin, strReadSipBin)
 
 									if tRes == nil then
@@ -842,7 +842,7 @@ function M.detect_secure_boot_mode(aArgs)
 		end -- readSipM2M image
 
 	else
-		strMsg = "Only romloader_uart and romloader_jtag are supported."
+		strMsg = "Only romloader_uart, romloader_eth and romloader_jtag are supported."
 	end -- if tPlugin
 
 	if iSecureBootStatus==SECURE_BOOT_DISABLED then
