8b0ea0d680bdb3443337a272375680310ad76e8e
gsathilscher <->
2024-07-12 15:07:23 +0200

    [build] improvements after review, added readme file and montest version file creation

--- gitVersionManager.py
+++ gitVersionManager.py
@@ -8,6 +8,7 @@ class gitVersionManager:
     devBranchFormat = r"^dev_v\d+\.\d+\.\d+$"
     devTagFormat = r"v\d+\.\d+\.\d+-dev\d+"
     releaseTagFormat = versionFormat
+    releaseBranchName = "master"
 
 
     # Creates a gitVersionManager Object
@@ -15,16 +16,6 @@ class gitVersionManager:
     # strRepoPath = path the current repo is in (use "/" for dirs, "." for current dir)
     def __init__(self, strRepoPath):
         self.repo = git.Repo(strRepoPath)
-
-
-    # Checks if current branch is a dev branch (matches the pattern "dev_vX.Y.Z", e.g. dev_v2.1.0)
-    def onDevBranch(self):
-        return bool(re.match(self.devBranchFormat, self.getCurrentBranch().name))
-
-
-    # Checks if the current branch is the master branch
-    def onMasterBranch(self):
-        return self.getCurrentBranch().name == "master"
 
 
     # Gets the branch the repository is currently on.
@@ -42,10 +33,29 @@ class gitVersionManager:
             return self.repo.active_branch
 
 
+    # Gets the current branch name
+    def getCurrentBranchName(self):
+        branch = self.getCurrentBranch()
+        # Remove "origin/" if present
+        if "/" in branch.name:
+            return str.split(branch.name, "/")[-1]
+        return branch.name
+
+
+    # Checks if current branch is a dev branch (matches the pattern "dev_vX.Y.Z", e.g. dev_v2.1.0)
+    def onDevBranch(self):
+        return bool(re.match(self.devBranchFormat, self.getCurrentBranchName()))
+
+
+    # Checks if the current branch is the release branch
+    def onReleaseBranch(self):
+        return self.getCurrentBranchName() == self.releaseBranchName
+
+
     # Gets the last tag from "git describe"-output
     #
     # Note that tags will be inherited when merging.
-    # When merging to master, create a release version tag ("vA.B.C")
+    # When merging to release branch, create a release version tag ("vA.B.C")
     # Only works with:
     # dev-tags:     "vA.B.C-devD"
     # release-tags: "vA.B.C"
@@ -54,10 +64,10 @@ class gitVersionManager:
         # Get the git describe output, gitpython has no good way of providing the tag
         description = self.repo.git.describe() # e.g. 'v2.1.0-dev13-15-gb39e454' or 'v2.1.0' when directly on tag
         if re.match(self.devTagFormat, description):
-            assert not self.onMasterBranch(), "Got dev tag but on master branch - forgot to set release tag?"
+            assert not self.onReleaseBranch(), "Got dev tag but on release branch - forgot to set release tag?"
             tagName = re.search(self.devTagFormat, description).group()
         elif re.match(self.releaseTagFormat, description):
-            assert self.onMasterBranch(), "Got release tag but on dev branch - forgot to set \"dev0\"-tag?"
+            assert self.onReleaseBranch(), "Got release tag but on dev branch - forgot to set \"dev0\"-tag?"
             tagName = re.search(self.releaseTagFormat, description).group()
         else:
             tagName = None
@@ -69,7 +79,9 @@ class gitVersionManager:
         sys.exit("Unable to find git tag that matches describe output")
 
 
-    # Gets the dev tag number, e.G. v2.1.0-dev13 will result in 13
+    # Gets the dev tag number
+    # 
+    # e.G. v2.1.0-dev13 will result in 13
     #
     # tag: tag the number should be parsed of
     def getDevTagNumber(self, tag):
@@ -77,7 +89,7 @@ class gitVersionManager:
         return int(re.findall(r'\d+', tag.name)[-1])
 
 
-    # Gets the number of commits since the tag was set
+    # Gets the number of commits made since the tag was set
     #
     # tag: the tag to start counting from
     def getCommitsSinceLastTag(self):
@@ -86,70 +98,87 @@ class gitVersionManager:
         return int(self.repo.git.rev_list('--count', f'{lastTagCommitHash}..{currentCommitHash}'))
 
 
-    # Creates a new dev tag with a new number
+    # Creates a new dev tag with a new number (vA.B.C-devD)
     # 
     # Will abort when not on a dev branch (name must be "dev_vA.B.C").
-    # Will only set a dev tag if there is not already one set on the current commit.
-    # Requires presence of "dev0"-tag on first dev branch commit (create it manually!).
-    # The dev tag number is the previous dev tag number increased by the number of commits
-    # since the last dev tag was set.
+    # Will abort if there is a tag already set on the current commit.
+    # Requires presence of "dev0"-tag on (first) dev branch commit (create it manually!).
+    # The dev tag number (D) is the previous dev tag number increased by the number of 
+    # commits since the last dev tag was set.
     def createDevTag(self):
-        if not self.onDevBranch():
-            sys.exit("Trying to create dev tag outside of dev branch, abort")
-        else:
-            lastTag = self.getLastTag()
-            lastDevTagNumber= self.getDevTagNumber(lastTag)
-            lastTagCommitHash = lastTag._get_commit().hexsha
-            currentCommitHash = self.repo.head.commit.hexsha
-            newTag = None
+        assert self.onDevBranch(), "Trying to create dev tag outside of dev branch, abort"
+        lastTag = self.getLastTag()
+        lastDevTagNumber= self.getDevTagNumber(lastTag)
+        lastTagCommitHash = lastTag._get_commit().hexsha
+        currentCommitHash = self.repo.head.commit.hexsha
+        newTag = None
 
-            # Do not set multiple dev tags on a single commit
-            if lastTagCommitHash != currentCommitHash:
-                # Create a new tag increased by the number of commits since last tag and generate project version string
-                latestDevTagVersion = re.search(self.versionFormat, lastTag.name).group()
-                newDevTagName = latestDevTagVersion + "-dev" + str(lastDevTagNumber + self.getCommitsSinceLastTag())
-                try:
-                    newTag = git.Tag.create(self.repo, newDevTagName, self.repo.head.commit, "Tag created automatically by flasher build process")
-                except:
-                    sys.exit(f'Could not create tag \"{newDevTagName}\" on commit \"{currentCommitHash}\"')
-            return newTag
+        # Do not set multiple dev tags on a single commit
+        assert lastTagCommitHash != currentCommitHash, "Tag already exists on current commit"
+
+        # Create a new tag increased by the number of commits since last tag and generate the project version string
+        latestDevTagVersion = re.search(self.versionFormat, lastTag.name).group()
+        newDevTagName = latestDevTagVersion + "-dev" + str(lastDevTagNumber + self.getCommitsSinceLastTag())
+
+        # Try to create the git tag on the current commit
+        try:
+            newTag = git.Tag.create(self.repo, newDevTagName, self.repo.head.commit, "Tag created automatically by flasher build process")
+        except:
+            sys.exit(f'Could not create tag \"{newDevTagName}\" on commit \"{currentCommitHash}\"')
+        return newTag
 
 
-    # Get the version number of the previous tag (e.g. "v2.1.0")
+    # Gets the version number of the last tag (e.g. "v2.1.0")
     #
-    # Aborts if the version can not be parsed.
+    # Aborts if there is no version in the last tag.
     def getVersionNumber(self):
         assert re.match(self.versionFormat, self.getLastTag().name), "Invalid format of last git tag"
         return re.search(self.versionFormat, self.getLastTag().name).group()
 
 
-    # Get the dev ending including commits since the last tag and the "repo-dirty"-"+".
+    # Gets the dev ending
     #
-    # Will omit "-devA" when on master branch (release)
+    # When on release branch:
+    # ""   when the current commit is the release tag commit
+    # "-X" when there were X commits since the last release tag
+    #
+    # When not on release branch:
     # "-devA-B+"
     # A = dev version from tag
     # B = commits since last tag
     # + : optional - will appear when the repo is dirty
     def getDevEnding(self):
-        if self.onMasterBranch():
+        if self.onReleaseBranch():
             ending = ""
+            if(self.getCommitsSinceLastTag() > 0):
+                ending += "-" + str(self.getCommitsSinceLastTag())
         else:
             ending = "-dev" + str(self.getDevTagNumber(self.getLastTag()))
-        ending += "-" + str(self.getCommitsSinceLastTag()) 
-        ending += "+" if self.repo.is_dirty() else ""
+            ending += "-" + str(self.getCommitsSinceLastTag()) 
         return ending
 
 
-    # Get the full Version string, format:
+    # Get the full Version string suitable for the current branch
     #
-    # vA.B.C-devD+-E-gHASH
+    # dev-branch or release branch: 
+    #  version + dev ending (from getDevEnding())
+    #  vA.B.C-devD-E
+    # 
+    # other (e.g. feature branch):
+    #  version + "-" + branch name + "-g" + short commit hash + repo-dirty-"+"
+    #  vA.B.C-BRANCH-gHASH+
+    # 
     # A.B.C = flasher version
     # D = dev tag version
-    # E = commits since last tag
-    # + : optional - will appear when the repo is dirty
+    # E = commits since last tag (optional)
+    # + : optional - inserted when the repo is dirty
     # HASH = shortened git hash of the current commit
     def getFullVersionString(self):
         name = self.getVersionNumber()
-        name += self.getDevEnding()
-        name += "g" + str(self.repo.head.commit.hexsha)[:7]
+        if self.onDevBranch() or self.onReleaseBranch():
+            name += self.getDevEnding()
+        else:
+            name += "-" + self.getCurrentBranchName()
+            name += "-g" + str(self.repo.head.commit.hexsha)[:7]
+            name += "+" if self.repo.is_dirty() else ""
         return name
