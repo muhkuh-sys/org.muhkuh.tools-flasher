4151a0621f70175f678a19fdd8d14e366f4b0104
fwoermann <fwoermann@hilscher.com>
2024-07-26 15:28:46 +0200

    [code] update romloader to release version

--- lua/lua/helper_files.lua
+++ lua/lua/helper_files.lua
@@ -102,7 +102,7 @@ local atHelperFileVersions = {
     {
         key = "start_mi",
         filename = "hboot_start_mi_netx90_com_intram.bin",
-        version = "Ver:GITv2.5.5-dev5-0",
+        version = "Ver:GITv2.5.5",
         version_offset = 0x0454
     },
 
