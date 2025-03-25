0e0b63f3c53d4a9b4ee04f9bb3f9f56d6b82615a
gsathilscher <->
2025-03-25 18:54:33 +0100

    Improve gitversionmanager and CI run triggers

--- gitVersionManager.py
+++ gitVersionManager.py
@@ -36,39 +36,35 @@ class gitVersionManager:
     def getCurrentBranch(self):
         print("Current repository wd: ", self.repo.working_dir)
         currentCommit = self.repo.head.commit
-
-        # Check for a "Detached Head State".
-        # This happens on pull request merge commits in GitHub or in submodules.
         
-
         # Check if there is a branch on the current commit. (Common at submodules)
         for branch in self.repo.remotes["origin"].refs:
             if self.repo.is_ancestor(currentCommit, branch.commit)\
                 or currentCommit == branch.commit:
-                print("Found branch with current commit: ", branch)
+                print("Found branch via current commit: ", branch)
                 return branch
 
+        # Check for a "Detached Head State".
+        # This happens on pull request merge commits in GitHub or in submodules.
         # Otherwise check if the current commit is a merge commit (more than 1 parent).
-        # (Common in GitHub pull request merge commits)
         if self.repo.head.is_detached or self.repo.active_branch is None:
             if len(currentCommit.parents) > 1:
                 # The first parent is the target branchs last commit.
                 # Since the source branch is relevant, use the second.
-                print("Current commit:", currentCommit)
-                print("Current commit parent 0: ", currentCommit.parents[0])
-                print("Current commit parent 1: ", currentCommit.parents[1])
-                print("Current repository wd: ", self.repo.working_dir)
                 for branch in self.repo.remotes["origin"].refs:
                     print("Checking Commit", branch.commit)
                     if currentCommit.parents[1] == branch.commit:
-                        print("Found branch with parent commit: ", branch)
+                        print("Found branch via parent commit: ", branch)
                         return branch
 
-                # TODO print error message if still no branch was found
-                print("ERROR: Branch could not be detected")
+            # If there is no current branch, throw an Exception with some details.
+            errorMsg = "ERROR: Branch could not be detected. "
+            errorMsg += f"Repository: {self.repo.working_dir}; "
+            errorMsg += f"Commit: {currentCommit}"
+            raise Exception(errorMsg)
 
-        else:
-            return self.repo.active_branch
+        # If the head is not detached and there is an active branch, return the active branch
+        return self.repo.active_branch
 
 
 
